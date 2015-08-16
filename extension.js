98f5546033b890b240c3275c03130ea74e60d16d
wara <kusabashira227@gmail.com>
2015-08-16 11:38:21 +0900

    Enable perpetuation

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@ const St = imports.gi.St;
 const GLib = imports.gi.GLib;
 const Main = imports.ui.main;
 const Lang = imports.lang;
+const Shell = imports.gi.Shell;
 const Clutter = imports.gi.Clutter;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -17,12 +18,14 @@ ShortMemo.prototype = {
 
     _init: function() {
         PanelMenu.Button.prototype._init.call(this, St.Align.START);
+        this.filePath = GLib.get_home_dir() + "/.shortmemo";
+
         this._buildUI();
+        this._refresh();
     },
 
     _buildUI: function() {
         this.message = new St.Label({
-            text:_("Hello"),
         });
         this.actor.add_actor(this.message);
 
@@ -44,16 +47,31 @@ ShortMemo.prototype = {
                 'key-press-event',
                 Lang.bind(this, function(o, e) {
                     if (e.get_key_symbol() == 65293) {
-                        this.menu.close();
-
                         let newText = this.newMessage.get_text();
-                        this.message.set_text(newText);
-                        this.newMessage.set_text('');
+                        this._save(newText)
+                        this._refresh()
                     }
                 }));
         this.mainBox.add_actor(this.newMessage);
 
         this.menu.box.add(this.mainBox);
+    },
+
+    _refresh: function() {
+        let text = this._load();
+        this.message.set_text(text);
+        this.newMessage.set_text("");
+        this.menu.close();
+    },
+
+    _save: function(text) {
+        GLib.file_set_contents(this.filePath, text);
+    },
+
+    _load: function() {
+        if (!GLib.file_test(this.filePath, GLib.FileTest.EXISTS))
+            GLib.file_set_contents(this.filePath, 'empty');
+        return Shell.get_file_contents_utf8_sync(this.filePath);
     },
 };
 
