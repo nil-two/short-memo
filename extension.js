39c56260d76446e238460756fc3bd1ee69bd1d63
kusabashira <kusabashira227@gmail.com>
2017-02-20 05:17:50 +0900

    Refactor around variable names

--- extension.js
+++ extension.js
@@ -15,17 +15,17 @@ const ShortMemo = new Lang.Class({
     _init: function() {
         PanelMenu.Button.prototype._init.call(this, St.Align.START);
         this._settings = Convenience.getSettings();
-        this._changedSignal = this._settings.connect(
+        this._settingsChangedSignal = this._settings.connect(
                 'changed',
                 Lang.bind(this, function(rettings, keys) {
-                    this._refresh();
+                    this._refreshUI();
                 }));
         this._buildUI();
-        this._refresh();
+        this._refreshUI();
     },
 
     _onDestroy: function() {
-        this._settings.disconnect(this._changedSignal);
+        this._settings.disconnect(this._settingsChangedSignal);
         this.parent();
     },
 
@@ -34,16 +34,16 @@ const ShortMemo = new Lang.Class({
         });
         this.actor.add_actor(this._message);
 
-        if (this._mainBox != null)
-            this._mainBox.destroy();
-        this._mainBox = new St.BoxLayout();
-        this._mainBox.set_vertical(true);
+        if (this._menuBox != null)
+            this._menuBox.destroy();
+        this._menuBox = new St.BoxLayout();
+        this._menuBox.set_vertical(true);
 
         let hint = new St.Label({
             name: "short-memo-hint",
             text:_("New message:"),
         });
-        this._mainBox.add_actor(hint);
+        this._menuBox.add_actor(hint);
 
         this._newMessage = new St.Entry({
             name: "short-memo-new-message",
@@ -55,33 +55,33 @@ const ShortMemo = new Lang.Class({
                 Lang.bind(this, function(o, e) {
                     if (e.get_key_symbol() == 65293) {
                         let newText = this._newMessage.get_text();
-                        this._save(newText);
-                        this._refresh();
+                        this._saveMessage(newText);
+                        this._refreshUI();
                     }
                 }));
-        this._mainBox.add_actor(this._newMessage);
+        this._menuBox.add_actor(this._newMessage);
 
-        this.menu.box.add(this._mainBox);
+        this.menu.box.add(this._menuBox);
     },
 
-    _refresh: function() {
-        let text = this._load();
-        this._message.set_text(text);
-        this._newMessage.set_text(text);
+    _refreshUI: function() {
+        let message = this._loadMessage();
+        this._message.set_text(message);
+        this._newMessage.set_text(message);
         this.menu.close();
     },
 
-    _save: function(text) {
-        this._settings.set_string(MESSAGE_KEY, text);
+    _saveMessage: function(message) {
+        this._settings.set_string(MESSAGE_KEY, message);
     },
 
-    _load: function() {
+    _loadMessage: function() {
         return this._settings.get_string(MESSAGE_KEY);
     },
 
-    setMessage: function(text) {
-        this._save(text);
-        this._refresh();
+    setMessage: function(message) {
+        this._saveMessage(message);
+        this._refreshUI();
     }
 });
 
